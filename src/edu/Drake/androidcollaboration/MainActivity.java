ab884c27cbd33068fb4570c4e5d2831f33388d8e
Joseph Bustamante <joseph.bustamante@drake.edu>
2013-02-21 01:11:05 -0600

    Added My portion of this collaborative project

--- src/edu/Drake/androidcollaboration/MainActivity.java
+++ src/edu/Drake/androidcollaboration/MainActivity.java
@@ -1,15 +1,33 @@
 package edu.Drake.androidcollaboration;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private static final String TAG = "MainActivity";
+	Button button;
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        button = (Button) findViewById(R.id.joesBtn);
+        button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.v(TAG, "Joes btn Pressed");
+				Intent intent = new Intent(v.getContext(), JoesActivity.class);
+				startActivity(intent);
+			}
+        });
     }
 
     @Override
